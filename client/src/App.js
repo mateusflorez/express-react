4b063a531a488563d928b94c76a19a31e178ecee
mateus.florez <bomberatox@gmail.com>
2022-12-17 22:24:13 -0300

    fetch data from api

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,19 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 function App() {
+
+  const [backendData, setBackendData] = useState()
+
+  useEffect(() => {
+    fetch("/api").then(
+      Response => Response.json()
+    ).then(
+      data => {
+        setBackendData(data)
+      }
+    )
+  }, [])
+
   return (
     <div>
 
